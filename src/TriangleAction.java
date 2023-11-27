872a0f0489cde910bd1b70604341309bcf6f11d0
Marlena Czyz <marlena.xv@gmail.com>
2023-11-27 12:28:05 +0100

    add class TriangleAction

---
+++ src/TriangleAction.java
@@ -0,0 +1,11 @@
+public class TriangleAction extends ActionBase {
+
+    @Override
+    public void execute(String[] paramArray){
+        int number = Integer.parseInt(paramArray[0]);
+        String sign = paramArray[1];
+        int color = Integer.parseInt(paramArray[2]);
+
+    }
+
+}
