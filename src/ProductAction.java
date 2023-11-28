d209b271bb346c145e49bc2b7f64257f971336d2
Marlena Czyz <marlena.xv@gmail.com>
2023-11-28 12:07:51 +0100

    add a condition to the price and small changes

--- src/ProductAction.java
+++ src/ProductAction.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ProductAction extends ActionBase{
@@ -13,14 +14,22 @@ public class ProductAction extends ActionBase{
                 String[] names = new String[number];
                 String[] prices = new String[number];
 
+                System.out.println("[in input - name, price]");
+
 
                 for (int i = 0; i < number; i++) {
                     System.out.println("Write name and price of " + (i + 1) + " product: ");
                     String user_string = input.nextLine();
 
-                    String[] parts = user_string.split(" "); //dzieli parametry
-                    name = parts[0]; //przypisuje nazwe do zmiennej
-                    price = parts[1]; //przypisuje cene do zmiennej
+                    String[] parts = user_string.split(", ");
+                    name = parts[0];
+                    price = parts[1];
+
+                    char pierwszyZnak = price.charAt(0);
+                    if (!Character.isDigit(pierwszyZnak)) {
+                        System.out.println("An incorrect price has been entered, you need to try again");
+                        return;
+                    }
 
                     names[i] = name;
                     prices[i] = price;
@@ -28,14 +37,14 @@ public class ProductAction extends ActionBase{
 
                 System.out.println("Your products: ");
                 for (int i = 0; i < number; i++) {
-                    System.out.println((i + 1) + ". " + names[i] + " " + prices[i]);
+                    System.out.println((i + 1) + ". " + names[i] + " - " + prices[i]);
                 }
             }else{
-                System.out.println("You need to provide one param - number");
+                System.out.println("You need to provide one parameter - number");
             }
 
         }catch (Exception e){
-            System.out.println("You need to provide one param - number");
+            System.out.println("Incorrect data");
         }
     }
 }
