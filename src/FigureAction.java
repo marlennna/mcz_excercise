d9851b1d42b108a75c3421cc02e5871f043e3f44
Marlena Czyz <marlena.xv@gmail.com>
2023-11-30 12:05:04 +0100

    add area and perimeter calculation in FigureAction and shapes in help

--- src/FigureAction.java
+++ src/FigureAction.java
@@ -1,38 +1,73 @@
-import java.security.spec.ECField;
-
 public class FigureAction extends ActionBase{
 
     @Override
     public void execute(String[] paramArray){
 
         String shape = paramArray[0];
+        double area, perimeter, a, b, c, d, h, d1, d2;
 
         try{
             switch (shape) {
                 case "triangle":
-                    System.out.println("triangle");
+                    a = Double.parseDouble(paramArray[1]);
+                    b = Double.parseDouble(paramArray[2]);
+                    c = Double.parseDouble(paramArray[3]);
+                    h = Double.parseDouble(paramArray[4]);
+                    area = 0.5*a*h;
+                    perimeter = a+b+c;
+                    System.out.println("TRIANGLE:\na: "+a+"\nb: "+b+"\nc: "+c+"\nh: "+h);
+                    System.out.println("Area: "+area+"\nPerimeter: "+perimeter);
                     break;
                 case "square":
-                    System.out.println("square");
+                    a = Double.parseDouble(paramArray[1]);
+                    System.out.println("SQUARE:\na: "+a);
+                    area = a*a;
+                    perimeter = 4*a;
+                    System.out.println("Area: "+area+"\nPerimeter: "+perimeter);
                     break;
                 case "rectangle":
-                    System.out.println("rectangle");
+                    a = Double.parseDouble(paramArray[1]);
+                    b = Double.parseDouble(paramArray[2]);
+                    System.out.println("RECTANGLE:\na: "+a+"\nb: "+b);
+                    area = a*b;
+                    perimeter = 2*a + 2*b;
+                    System.out.println("Area: "+area+"\nPerimeter: "+perimeter);
                     break;
                 case "rhombus":
-                    System.out.println("Rhombus");
+                    a = Double.parseDouble(paramArray[1]);
+                    h = Double.parseDouble(paramArray[2]);
+                    System.out.println("RHOMBUS:\na: "+a+"\nh: "+h);
+                    area = a*h;
+                    perimeter = 4+a;
+                    System.out.println("Area: "+area+"\nPerimeter: "+perimeter);
                     break;
                 case "trapezoid":
-                    System.out.println("Trapezoid");
+                    a = Double.parseDouble(paramArray[1]);
+                    b = Double.parseDouble(paramArray[2]);
+                    c = Double.parseDouble(paramArray[3]);
+                    d = Double.parseDouble(paramArray[4]);
+                    h = Double.parseDouble(paramArray[5]);
+                    System.out.println("TRAPEZOID: \na: "+a+"\nb: "+b+"\nc: "+c+"\nd: "+d+"\nh: "+h);
+                    area = a+b*h/2;
+                    perimeter = a+b+c+d;
+                    System.out.println("Area: "+area+"\nPerimeter: "+perimeter);
                     break;
                 case "kite":
-                    System.out.println("kite");
+                    a = Double.parseDouble(paramArray[1]);
+                    b = Double.parseDouble(paramArray[2]);
+                    d1 = Double.parseDouble(paramArray[3]);
+                    d2 = Double.parseDouble(paramArray[4]);
+                    System.out.println("KITE: \na: "+a+"\nb: "+b+"\nd1: "+d1+"\nd2: "+d2);
+                    area = 0.5*d1*d2;
+                    perimeter = 2*a+2*b;
+                    System.out.println("Area: "+area+"\nPerimeter: "+perimeter);
                     break;
                 default:
-                    System.out.println("You chose incorrect figure");
+                    System.out.println("You chose incorrect figure (informations in help)");
 
             }
         }catch (Exception e){
-            System.out.println(e);
+            System.out.println("You need to provide the appropriate number of parameters - numbers (informations in help)");
         }
 
 
--- src/HelpAction.java
+++ src/HelpAction.java
@@ -2,7 +2,8 @@ public class HelpAction extends ActionBase{
     @Override
     public void execute(String[] paramsArray) {
 //        System.out.println("Pomoc - Tu np nie ma zadnych parametrow wiec nie korzystamy z dostarczonych");
-        System.out.println("You can write: \n-> help \n-> add \n-> sum \n-> sub (subtraction) \n-> triangle \n->qe (quadratic equation) \n-> quit");
+        System.out.println("You can write: \n-> help \n-> add \n-> sum \n-> sub (subtraction) \n-> triangle \n-> qe (quadratic equation) \n-> square \n-> quit");
         System.out.println("\nColors: white, black, blue, cyan, red, purple, green, yellow, gray");
+        System.out.println("\nShapes and its parameters: triangle a b c h, square a, rectangle a b, rhombus a h, trapezoid a b c d h, kite a b d1 d2");
     }
 }
